40273e3f2c85a77d201db63a3e410dfd75ba4ff3
uuuoop <1434046860@qq.com>
2021-08-11 12:29:58 +0800

    默认的

--- src/component/app-header/index.js
+++ src/component/app-header/index.js
@@ -1,18 +1,44 @@
 import React, { memo } from 'react';
 import { NavLink } from 'react-router-dom';
 
+import { headerLinks } from '@/common/local-data';
+
 import {
   HeaderWrapper,
   HeaderLeft,
   HeaderRight
 } from './style';
 
-export default memo(function WAppHeader() {
+export default memo(function WAppHeader () {
+
+  const showSelectItem=(val, index)=>{
+    if (index < 3) {
+      return (
+        <NavLink to={val.link}>{val.title}</NavLink>
+      );
+    } else {
+      return (
+        <a href={val.link}>{val.title}</a>
+      );
+    }
+  }
+
   return (
     <HeaderWrapper>
       <div className={'content wrap_v1'}>
         <HeaderLeft>
-          <a href="#/" className={"logo sprite_01"}></a>
+          <a href="#/" className={'logo sprite_01'}/>
+          <div className={'select-list'}>
+            {
+              headerLinks.map((val, index) => {
+                return (
+                  <div key={val.title} className="select-item">
+                    {showSelectItem(val, index)}
+                  </div>
+                );
+              })
+            }
+          </div>
         </HeaderLeft>
         <HeaderRight>right</HeaderRight>
       </div>
--- src/component/app-header/style.js
+++ src/component/app-header/style.js
@@ -6,11 +6,11 @@ export const HeaderWrapper = styled.div`
 
   .content {
     height: 70px;
-    background-color: gray;
-    
+    //background-color: gray;
+
     display: flex;
     justify-content: space-between;
-    align-items: center;
+    //align-items: center;
   }
 
   .divider {
@@ -20,15 +20,65 @@ export const HeaderWrapper = styled.div`
 `;
 
 export const HeaderLeft = styled.div`
+  display: flex;
+
   .logo {
     display: block;
     width: 176px;
     height: 69px;
     background-position: 0 0;
   }
-  
+
+  .select-list {
+    display: flex;
+    align-items: center;
+    //line-height: 70px;
+
+    .select-item {
+      position: relative;
+
+      a {
+        display: block;
+        color: #ccc;
+        padding: 0 20px;
+      }
+
+      :last-of-type {
+        position: relative;
+
+        ::after {
+          position: absolute;
+          content: "";
+          width: 28px;
+          height: 19px;
+          background-image: url(${require('@/assets/img/sprite_01.png')});
+          background-position: -190px 0;
+          top: 20px;
+          right: -15px;
+        }
+      }
+
+      &:hover a, a.active {
+        color: #fff;
+        background: #000;
+        text-decoration: none;
+      }
+
+      .active .icon {
+        position: absolute;
+        display: inline-block;
+        width: 12px;
+        height: 7px;
+        bottom: -1px;
+        left: 50%;
+        transform: translate(-50%, 0);
+        background-position: -226px 0;
+      }
+    }
+  }
+
 `;
 
 export const HeaderRight = styled.div`
-  
-`
+
+`;
